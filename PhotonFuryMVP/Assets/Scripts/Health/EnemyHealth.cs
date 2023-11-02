cb3498187b07616d1be1d76cbf43440064c71f6d
LlamahatVR <llamahatmedia@gmail.com>
2023-11-02 16:42:44 +0000

    Enemy Death Particle Effect - Boom

--- PhotonFuryMVP/Assets/Scripts/Health/EnemyHealth.cs
+++ PhotonFuryMVP/Assets/Scripts/Health/EnemyHealth.cs
@@ -9,6 +9,7 @@ namespace photonfury.health
     public class EnemyHealth : Health
     {
         [SerializeField] LevelProgressManager lvlPrgsManager;
+        [SerializeField] private ParticleSystem _particleSystem;
 
         // Start is called before the first frame update
         protected override void Start()
@@ -31,10 +32,10 @@ namespace photonfury.health
         public override void Death()
         {
             base.Death();
-
+            var transform1 = transform;
+            Instantiate(_particleSystem, transform1.position, transform1.rotation);
             var parent = gameObject.transform.parent.gameObject;
             parent.SetActive(false);
-
             lvlPrgsManager.CheckEnemyStatus();
         }
     }
